9cbc6d60a2ba0aeb7d1ecd68ea829ab8b0cf2e57
yangBasak <yangbasak@gamil.com>
2022-12-22 23:47:25 +0900

    구구단 마무리, 웹팩 설정 배우는 중

---
+++ 끝말잇기/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'wordChainGame-setting', // 어떤 파일을 위한 웹팩 설정인가에 대한거
+    mode: 'development', // 실서비스에서는 production으로 바꿈
+    devtool: 'eval', // 빠르게 하겠대 
+    // 입력
+    entry: {
+
+    },
+    // 출력
+    output: {
+
+    }
+}
