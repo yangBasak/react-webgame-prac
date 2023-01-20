f36339e65198bcc6ed9e9559aa531c3bad16413c
yangBasak <yangBasak@gmail.com>
2023-01-20 18:23:49 +0900

    중첩 라우팅 수정중

--- dev/gameMatcher.jsx
+++ dev/gameMatcher.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, Route, Routes } from "react-router-dom";
 
 import MineSerach from "./gameComponent/mineSerach";
 import LottoGame from "./gameComponent/lottoGameHooks";
@@ -7,15 +7,24 @@ import LottoGame from "./gameComponent/lottoGameHooks";
 function GameMatcher() {
   let params = useParams();
   let location = useLocation();
+  console.log(location);
+  console.log(params);
+  // if (params.name === "mineSerach") {
+  //   return <MineSerach />;
+  // }
+  // if (params.name === "lottoGame") {
+  //   return <LottoGame />;
+  // } else {
+  //   return <div>일치하는 게임 없음</div>;
+  // }
 
-  if (params.name === "mineSerach") {
-    return <MineSerach />;
-  }
-  if (params.name === "lottoGame") {
-    return <LottoGame />;
-  } else {
-    return <div>일치하는 게임 없음</div>;
-  }
+  return (
+    <Routes>
+      <Route path="mineSerach" element={<MineSerach />}></Route>
+      <Route path="lottoGame" element={<LottoGame />}></Route>
+      <Route path="*" element={<div>일치하는 게임 없음</div>}></Route>
+    </Routes>
+  );
 }
 
 export default GameMatcher;
--- dev/games.jsx
+++ dev/games.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import GameMatcher from "./gameMatcher";
 
 const Games = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <div>
         <Link to="/game/mineSerach">마인</Link>
         <Link to="/game/lottoGame">로또</Link>
         <Link to="/game/index">게임매처</Link>
       </div>
       <Routes>
-        <Route path="/game/:name" element={<GameMatcher />}></Route>
-        <Route path="/game/mineSerach" element={<GameMatcher />}></Route>
+        <Route path="/game/:name/*" element={<GameMatcher />}></Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 export default Games;
