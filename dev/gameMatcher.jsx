cc1a91b825cd54505b48c0e7d82b6bc9d4b1d324
yangBasak <yangBasak@gmail.com>
2023-02-27 00:31:05 +0900

    1번~2번
    2번

--- dev/gameMatcher.jsx
+++ dev/gameMatcher.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, useLocation, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import MineSerach from "./gameComponent/mineSerach";
 import LottoGame from "./gameComponent/lottoGameHooks";
